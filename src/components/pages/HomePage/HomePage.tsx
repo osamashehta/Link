daca9a646a9f0a8faa2146064f6a51c36ae48f0e
osama shehta <osama.shehtafathi@gmail.com>
2025-04-14 23:04:12 +0200

    type

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -1,15 +1,11 @@
-'use client'
 
-import { userStore } from "@/stores/user-store";
 
 
 const HomePage = () => {
-  const user  = userStore((state:any) => state.user);
-  console.log("user,", user);
   
    return (
     <>
-      <div>{user.name}</div>
+      <div>test</div>
     
     </>
    )
