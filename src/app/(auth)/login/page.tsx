0c1fb81ac46b58c7047d48a723ec51d0e166c541
osama shehta <osama.shehtafathi@gmail.com>
2025-04-25 20:12:34 +0300

    Implement token check in login and signup pages to redirect authenticated users

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,16 @@
 import LoginPage from "@/components/pages/LoginPage/LoginPage";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const page = () => {
+const page = async() => {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token")?.value || "";
+
+  if (token) {
+    redirect("/");
+  }
+
   return (
     <>
       <LoginPage />
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,6 +1,15 @@
 import SignUpPage from "@/components/pages/SignUpPage/SignUpPage";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-const page = () => {
+const page = async() => {
+
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token")?.value || "";
+
+  if (token) {
+    redirect("/");
+  }
   return <SignUpPage />;
 };
 
